fe3adf4aba4f3fb677ca5cf6546443671ac21276
Bill Hilke <whilke@gmail.com>
2015-11-16 11:02:33 -0800

    + Add project structure. + Add json config file system

---
+++ Asgard.Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Asgard.Server")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("enBask Studios")]
+[assembly: AssemblyProduct("Asgard.Server")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b9997944-fc41-4832-91b1-0e8c7f4cecd0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("Asgard.Server.Tests")]
+[assembly: InternalsVisibleTo("Asgard.Server.Explorables")]
+
---
+++ Asgard.Server/Utils/Config.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asgard.Utils
+{
+    /// <summary>
+    /// Thread safe JSON config system
+    /// </summary>
+    public sealed class Config
+    {
+        private static string _configFile = "config.json";
+        private static bool _loaded = false;
+        private static object _lockObject = new object();
+        private static JObject _jsonObject = null;
+
+        #region Private Static
+        /// <summary>
+        /// lazy load config data if consumer doesn't override
+        /// </summary>
+        private static void EnsureLoaded()
+        {
+            if (!_loaded) //quick optimization around locking once loaded
+            {
+                lock (_lockObject)
+                {
+                    if (!_loaded) //check for race condition since first check was outside
+                                  //lock
+                    {
+                        _loaded = true;
+                        Load();
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region Public Static
+        /// <summary>
+        /// Overrides config file with consumer supplied file name
+        /// </summary>
+        /// <param name="config"></param>
+        public static void Load(string config = "")
+        {
+            if (!string.IsNullOrEmpty(config))
+            {
+                _configFile = config;
+            }
+
+            if (!File.Exists(_configFile))
+            {
+                _jsonObject = new JObject();
+                return;
+            }
+
+            var rawJson = File.ReadAllText(_configFile);
+            _jsonObject = JObject.Parse(rawJson);
+
+            if (_jsonObject == null)
+            {
+                throw new InvalidDataException("bad config data");
+            }
+        }
+
+        public static string GetString(string key)
+        {
+            if (String.IsNullOrEmpty(key))
+            {
+                throw new ArgumentNullException();
+            }
+
+            EnsureLoaded();
+
+            JToken token = null;
+            var tokenFound = _jsonObject.TryGetValue(key, out token);
+            if (!tokenFound)
+            {
+                return String.Empty;
+            }
+
+            return token.ToString();
+        }
+
+        public static int GetInt(string key)
+        {
+            var result = GetString(key);
+            Int32 intResult;
+            bool parseResult = Int32.TryParse(result, out intResult);
+            if (!parseResult)
+            {
+                return 0;
+            }
+
+            return intResult;
+        }
+
+        public static T Get<T>(string key)
+        {
+            var json = GetString(key);
+            T result = JsonConvert.DeserializeObject<T>(json);
+            return result;
+        }
+        #endregion
+
+    }
+}
